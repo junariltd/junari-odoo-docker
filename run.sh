9bbfe3e823cc49fe04170bb043dba9a678c74f91
Russell Briggs <russell.briggs@junari.com>
2020-06-14 21:53:59 +0100

    Store data & sessions in a junari-odoo-data volume

--- run.sh
+++ run.sh
@@ -2,9 +2,5 @@
 set -x
 
 docker run --rm -it \
+    -v junari-odoo-data:/opt/odoo/data \
     -p 8069:8069 --env-file=./odoo.env junari-odoo $@
-
-    # -v $PWD/config:/etc/odoo \
-    # -v $PWD/odoo:/opt/odoo/odoo \
-    # -v $PWD/custom_addons:/opt/odoo/custom_addons \
-    # -v $PWD/data:/opt/odoo/data \
