3acca089e760876083f8e420a8570d622ce9f7da
Russell Briggs <russ@paraflyer.net>
2021-07-15 11:54:24 +0100

    [T13653] Parameterise odoo version + revision

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -x
 
+ODOO_VERSION=13.0
+
 docker run --rm -it \
     -v junari-odoo-data:/opt/odoo/data \
-    -p 8069:8069 --env-file=odoo.env junari/odoo $@
+    -p 8069:8069 --env-file=odoo.env junari/odoo:$ODOO_VERSION $@
